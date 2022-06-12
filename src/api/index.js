d3249a289c6cbc042aecd2db68207338fafd19ae
Andrej Boleslavsky <yer@atlas.cz>
2022-06-12 04:22:07 +0200

    Mainnet OpenSea API calls

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ export const getNftTokens = async (ownerAddress) => {
   const CONFIG = await getConfigData();
 
   const res = fetch(
-    `https://testnets-api.opensea.io/api/v1/assets?owner=${ownerAddress}&asset_contract_address=${CONFIG.CONTRACT_ADDRESS}&order_direction=desc&offset=0&limit=50&collection=nfpx-2-v2`,
+    `https://api.opensea.io/api/v1/assets?owner=${ownerAddress}&asset_contract_address=${CONFIG.CONTRACT_ADDRESS}&order_direction=desc&offset=0&limit=50&collection=NFPX`,
     { method: "GET", headers: { Accept: "application/json" } }
   ).then(response => response.json()).then(({ assets }) => {
     return assets;
