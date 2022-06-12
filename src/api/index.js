f2c481bad0251715a08c84105cc197717b6b681f
Andrej Boleslavsky <yer@atlas.cz>
2022-06-12 20:16:21 +0200

    OpenSea API - CORS issue, catch and configuration

--- src/api/index.js
+++ src/api/index.js
@@ -4,11 +4,14 @@ export const getNftTokens = async (ownerAddress) => {
   const CONFIG = await getConfigData();
 
   const res = fetch(
-    `https://api.opensea.io/api/v1/assets?owner=${ownerAddress}&asset_contract_address=${CONFIG.CONTRACT_ADDRESS}&order_direction=desc&offset=0&limit=50&collection=NFPX`,
+    `https://api.opensea.io/api/v1/assets?owner=${ownerAddress}&asset_contract_address=${CONFIG.CONTRACT_ADDRESS}&order_direction=desc&offset=0&limit=50&collection=nfpx-2-v2`,
     { method: "GET", headers: { Accept: "application/json" } }
   ).then(response => response.json()).then(({ assets }) => {
     return assets;
-  })
+  }).catch(function(error) {
+    console.log('Request failed', error)
+    return [];
+});
 
   return res;
 }
