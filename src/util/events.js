ea27bec4666bff509badf04a468d6123467d6352
Andrej Boleslavsky <yer@atlas.cz>
2022-06-17 05:47:35 +0200

    Cache busting

--- src/util/events.js
+++ src/util/events.js
@@ -1,7 +1,10 @@
 export const getEvents = async () => {
   const url = 'https://npxs.herokuapp.com/data/leaderboard.json';
 
-  const holdersData = await fetch(url).then((response) => response.json());
+  const holdersData = await fetch(url + '?'+(new Date()).getTime(), {
+    headers: {
+      'Cache-Control': 'no-cache'
+    }}).then((response) => response.json());
   
   return holdersData["holders"];
 }
