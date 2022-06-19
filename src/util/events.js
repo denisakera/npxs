eef366da9b1adbc9654c99459dd6ba356f4a0a32
Andrej Boleslavsky <yer@atlas.cz>
2022-06-19 03:15:17 +0200

    Github.io data fetch

--- src/util/events.js
+++ src/util/events.js
@@ -1,10 +1,11 @@
 export const getEvents = async () => {
   const url = 'https://cryptopintxos.github.io/leaderboard.json';
 
-  const holdersData = await fetch(url + '?'+(new Date()).getTime(), {
-    headers: {
-      'Cache-Control': 'no-cache'
-    }}).then((response) => response.json());
+
+
+  const holdersData = await fetch(url).then((response) => response.json());
+
+
   
   return holdersData["holders"];
 }
